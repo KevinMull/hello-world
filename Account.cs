12699d0b2fc1069d29044642c2c9f35a092b08ba
TrendCare-Kevin <38513114+TrendCare-Kevin@users.noreply.github.com>
2020-07-08 11:43:32 +0100

    commit html

---
+++ Account.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for public class Account 
+/// </summary>
+public class Account
+{
+    private static decimal minIncome;
+    private static int minAge;
+
+    public static bool AccountAllowed(decimal income, int age)
+    { if ((income >= minIncome) && (age >= minAge))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
